21ef672a70b17b404db47d52ec98243239c47740
KT <119061015+KTTU31@users.noreply.github.com>
2023-05-22 04:51:53 +0800

    Update LASER.v

--- 2023/univ/version1/LASER.v
+++ 2023/univ/version1/LASER.v
@@ -7,7 +7,7 @@
 //
 // ABSTRACT:  IC contest 2023
 // DESIGN VERSION: 1
-// DESCRIPITON: cycle = 
+// DESCRIPITON: cycle = 25566
 //              area  = 
 // MODIFIED: 將input的data儲存時採用shift register
 //
